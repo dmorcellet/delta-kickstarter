f4c0c6489f9dd25a28bd6dfb5aba128f4d54ea8a
Damien Morcellet <dmorcellet@free.fr>
2021-06-18 11:43:42 +0200

    Initial commit

---
+++ src/main/java/delta/kickstarter/Kickstarter.java
@@ -0,0 +1,52 @@
+package delta.kickstarter;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
+import delta.launcher.Launcher;
+import delta.launcher.data.LauncherConfiguration;
+import delta.launcher.data.io.xml.LauncherConfigurationXmlIO;
+import delta.updates.engine.UpdateController;
+
+/**
+ * Application launcher.
+ * Launches a main class using a classpath specified in a XML file.
+ * @author DAM
+ */
+public class Kickstarter
+{
+  private static final Logger LOGGER=Logger.getLogger(Kickstarter.class);
+
+  private static void checkForUpdates()
+  {
+    UpdateController ctrl=new UpdateController();
+    File rootAppDir=new File(".");
+    ctrl.doIt(rootAppDir);
+  }
+
+  private static void startApp()
+  {
+    LOGGER.info("Starting app!");
+    File configFile=new File("launcher.xml");
+    LauncherConfiguration config=LauncherConfigurationXmlIO.parseFile(configFile);
+    Launcher.launch(config);
+  }
+
+  /**
+   * Main method for this test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    try
+    {
+      checkForUpdates();
+      startApp();
+    }
+    catch(Throwable t)
+    {
+      LOGGER.error("Caught fatal exception!", t);
+    }
+  }
+}
---
+++ src/test/java/delta/kickstarter/MainTestKickstarter.java
@@ -0,0 +1,17 @@
+package delta.kickstarter;
+
+/**
+ * A simple kickstarter test.
+ * @author DAM
+ */
+public class MainTestKickstarter
+{
+  /**
+   * Main method for this test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    Kickstarter.main(args);
+  }
+}
